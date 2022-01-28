c0b83756bd49a5c4e0e3e2956dc6e339c988f5b1
Tamirat Fisseha <tamiratfissehasitotaw@gmail.com>
2022-01-28 15:39:33 -0600

    first commit

--- src/main/java/com/cs544/videorating/videoservice/VideoServiceApplication.java
+++ src/main/java/com/cs544/videorating/videoservice/VideoServiceApplication.java
@@ -2,8 +2,10 @@ package com.cs544.videorating.videoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class VideoServiceApplication {
 
     public static void main(String[] args) {
